52e087cb1847238dbddc867702f01c8f162deb77
haguo2008 <haguo@21cn.com>
2020-03-25 10:27:01 +0800

    1.0

---
+++ woo-product-categories-log.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Plugin Name: WooCommerce 产品分类目录
+ * Description: 产品页面显示产品分类目录
+ */
+?>
+<?php
+add_action( 'wp_enqueue_scripts', 'changjili_product_categories_css' );
+function changjili_product_categories_css() {
+    wp_register_style( 'changjili_product_categories_css', plugins_url( 'css/style.css', __FILE__ ) );
+    wp_enqueue_style( 'changjili_product_categories_css' );
+}
+ 
+add_action( 'woocommerce_before_shop_loop', 'changjili_product_subcategories', 50 );
+function changjili_product_subcategories( $args = array() ) {
+	global $wp;
+	$woocommerce_category_id = get_terms('product_type');
+	echo '<div class="section section-filters">';
+	echo '<div class="section_wrapper clearfix"> <!-- #Filters -->';
+	echo '<div id="Filters" class="column one ">';
+	echo '<ul class="filters_buttons">';
+	echo '<li class="label">Filter by</li>';
+	echo '<li class="categories"><a class="open" href="#"><i class="icon-docs"></i>Product Categories</a></li>';
+	echo '</ul>';
+		foreach ($woocommerce_category_id as $term) {
+			 
+			$args = array( 'parent' => $woocommerce_category_id ->term_id );
+			$terms = get_terms( 'product_cat', $args );
+			 if ( $terms ) {
+				echo '<div class="filters_wrapper">';
+				echo '<ul class="categories">';
+				echo '<li class="reset-inner"><a data-rel="*" href="'.home_url( $wp->request.'/' ).'">All</a></li>';
+			foreach ( $terms as $term ) {
+					  //var_dump($term->count);
+				echo '<li class="woocommerce-product-category-page">';
+				echo '<a href="' .  esc_url( get_term_link( $term ) ) . '" class="' . $term->slug . '">';
+				echo $term->name;
+				echo '</a>';
+				echo '</li>';
+			}
+				echo '<li class="close"><a href="#"><i class="icon-cancel"></i></a></li>';
+				echo '</ul>';
+				echo '</div>';
+		}
+
+	}
+	 echo '</div>';
+	 echo '</div>';
+	 echo '</div>';
+}
+	 
+	 
